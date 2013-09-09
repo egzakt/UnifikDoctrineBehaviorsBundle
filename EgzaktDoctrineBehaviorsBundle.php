166964b79996b356067952cfb2ca2553947e2c7d
Jean-François Côté <tioiscote@gmail.com>
2013-09-09 10:12:45 -0400

    Initial config of the bundle

---
+++ EgzaktDoctrineBehaviorsBundle.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Egzakt\DoctrineBehaviorsBundle;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class EgzaktSystemBundle extends Bundle
+{
+    public function build(ContainerBuilder $container)
+    {
+
+    }
+}
